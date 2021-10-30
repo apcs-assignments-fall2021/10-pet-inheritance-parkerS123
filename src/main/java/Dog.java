fc825ff7a2c8923f8490e04c85d97f754d0e9a36
parkerschwantes <23pschwantes@athenian.org>
2021-10-30 11:17:45 -0700

    one thing about the test isn't passing

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,40 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String Breed;
 
 
     // Constructors
-    
+    public Dog(String name, int age, String Breed){
+        super(name, age);
+        this.Breed = Breed;
+    }
+
+    public Dog(){
+        super("milan", 16);
+        this.Breed = "Chihuahua";
+    }
 
     // makeNoise() method
+    public void makeNoise(){
+        System.out.println("woof woof");
+    }
 
 
     // toString method
+    public String toString(){
+        String s = "Dog information: " + getName() + " , Age: " + getAge() + " , Breed: " + this.Breed;
+        return s;
+    }
 
 
     // Getter
+    public String getBreed(){
+        return this.Breed;
+    }
 
 
     // Setter
+    public void setBread(String Breed){
+        this.Breed = Breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,14 @@
 public class DogClient {
     public static void main(String[] args) {
+        Dog goodboy = new Dog("Pippen", 2, "French Bulldog");
+
+        System.out.println(goodboy.getBreed());
+        goodboy.setAge(4);
+        System.out.println(goodboy.getAge());
+        goodboy.setBread("Pug");
+        System.out.println(goodboy.getBreed());
+        System.out.println(goodboy.toString());
+
         
     }
 }
